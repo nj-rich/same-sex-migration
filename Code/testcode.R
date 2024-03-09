fa3d9d1b2ee98a9975f03acbd82d94f3bf717ccb
nj-rich <njrich@umich.edu>
2024-03-08 21:50:31 -0500

    GOT MERGE TO WORK WITH ARROW YAY

--- Code/testcode.R
+++ Code/testcode.R
@@ -1,4 +1,11 @@
 #messing with arrow to better load stuff REMEMBER COLLECT FOR COMPUTATIONS
+library(tidyverse)
+
+#to help deal with large data files
+#install.packages("data.table") #for fread
+#library(data.table)
+install.packages("arrow") #for parquet
+library(arrow)
 
 #loading data
 pull1_csv <- open_dataset(
@@ -11,26 +18,31 @@ pull1_csv <- open_dataset(
 #note: can stay here but parquet might help
 
 #make parquet
-pq_path <- "/Users/njrich/Desktop/Econ495/same-sex-migration/Data/pull1"
+#pq_path <- "/Users/njrich/Desktop/Econ495/same-sex-migration/Data/pull1/parquet"
 
-pull1_csv %>%
-  group_by(YEAR) %>%
-  write_dataset(path = pq_path, format = "parquet")
+#pull1_csv %>%
+#  group_by(YEAR) %>%
+#  write_dataset(path = pq_path, format = "parquet")
 
 #open parquet
-pull1_pq <- open_dataset(pq_path)
+#pq_path <- "/Users/njrich/Desktop/Econ495/same-sex-migration/Data/pull1/parquet"
+#pull1_pq <- open_dataset(pq_path)
 
-df_pq <- pull1_pq
+#df_pq <- pull1_pq
+
+#maybe no need parquet?
 
 #test using/making/IDing partners
-#ok this is currently broken but making progress 
-#see error:
-# Error in `compute.arrow_dplyr_query()`:
-#   ! Invalid: Could not open Parquet input source '/Users/njrich/Desktop/Econ495/same-sex-migration/Data/pull1/codebook_pull1.xml': Parquet magic bytes not found in footer. Either the file is corrupted or this is not a parquet file.
-# Run `rlang::last_trace()` to see where the error occurred.
 
-partner <- pull1_pq %>%
-  select(YEAR, SERIAL, PERNUM, SEX) %>%
-  collect()
+partner <- pull1_csv %>%
+  select(YEAR, SERIAL, PERNUM, SEX) %>% #rename sex var for clarity?
+  compute()
+
+  
+df_partners <- pull1_csv %>%
+  left_join(partner, join_by(YEAR, SERIAL, SPLOC == PERNUM)) %>% #WATCH CLARITY WHERE SPLOC IS
+  compute()
+
+#AND BOOM WE HAVE IT YAY ARROW WATCH IF MERGE WORKED (AND CHECK)
 
 
