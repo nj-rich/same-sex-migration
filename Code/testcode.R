5270a3bfa7084aa4c02bc26e7129ee267b6b28d4
Rich <njrich@UMICH.EDU>
2024-09-20 11:42:43 -0400

    identified bug in W24 code, see comments on bottom of test code file

--- Code/testcode.R
+++ Code/testcode.R
@@ -31,6 +31,7 @@ df_state_years <- df_samesex %>%
   mutate(state_year = paste(state_name, YEAR, sep = "_"))
 
 state_years <- df_state_years$state_year %>% unique()
+rm(df_state_years)
 
 #outer function (watch opposite sex df differences)
 create_output <- function(input_df) {
@@ -42,10 +43,10 @@ df <- input_df %>%
 
 #create export file paths
 df_name_string <- deparse(substitute(input_df)) #convert df name to string for later file export
-csv_filepath <- paste("C:\\Users\\njrich\\Desktop\\same-sex-migration\\W24_builds\\F24_validation\\", gsub("df_samesex_", "", df_name_string), ".csv")
-stata_filepath <- paste("C:\\Users\\njrich\\Desktop\\same-sex-migration\\W24_builds\\F24_validation\\", gsub("df_samesex_", "", df_name_string), ".dta")
+csv_filepath <- paste("C:\\Users\\njrich\\Desktop\\same-sex-migration\\W24_builds\\F24_validation\\", gsub("df_samesex_", "", df_name_string), ".csv", sep = "")
+stata_filepath <- paste("C:\\Users\\njrich\\Desktop\\same-sex-migration\\W24_builds\\F24_validation\\", gsub("df_samesex_", "", df_name_string), ".dta", sep = "")
 
-#inner function building export row by row- add if statement for stateyears not present
+#inner function building export row by row
 
 build_samesex_rows <- function(input_stateyear) {
 small_df = df
@@ -56,7 +57,8 @@ indv_state = substr(input_stateyear, 1, 2)
 indv_year = substr(input_stateyear, 4, 8) %>%
   as.numeric()
 
-if((indv_state %in% small_df$state_name) & (indv_year %in% small_df$YEAR)) {
+if((indv_state %in% small_df$state_name) & (indv_year %in% small_df$YEAR)) { #output fraction IF have same-sex individuals; MOVE THIS IF STATEMENT DOWN, 
+
 #count total potential migrants to state (all same-sex indv in given year excluding those in destination state)
 tot_pot_mig_build <- small_df %>%
   filter(YEAR == indv_year) %>%
@@ -76,6 +78,8 @@ mig_to_state_build <- small_df %>%
 mig_to_state = mig_to_state_build$count[1]
 
 #make fraction
+
+#MOVE IF STATEMENT DOWN HERE - FOR MM NA
 rate = mig_to_state/tot_pot_mig
 }
 #for missing state-years
@@ -127,13 +131,46 @@ write_dta(output, stata_filepath)
 return(output)
 }
 
-test2 <- create_output(df_samesex_mm)
-#function not actually faster? intriguing.
-#still need to rCompare
+#test differences with original method
+F24_mm_m <- create_output(df_samesex_mm_m)
+F24_mm_f <- create_output(df_samesex_mm_f)
+F24_mm_gen <- create_output(df_samesex_mm_gen)
 
+mod_F24_mm_m <- F24_mm_m %>%
+  filter(!is.na(staterate_year)) %>%
+  mutate(stateyear = paste(YEAR, state_name))
+mod_F24_mm_f <- F24_mm_f %>%
+  filter(!is.na(staterate_year)) %>%
+  mutate(stateyear = paste(YEAR, state_name))
+mod_F24_mm_gen <- F24_mm_gen %>%
+  filter(!is.na(staterate_year)) %>%
+  mutate(stateyear = paste(YEAR, state_name))
 
+W24_mm_m <- read_csv("C:\\Users\\njrich\\Desktop\\same-sex-migration\\W24_builds\\mm_m.csv") %>%
+  select(YEAR:time_leg_chunked) %>%
+  mutate(stateyear = paste(YEAR, state_name))
+W24_mm_f <- read_csv("C:\\Users\\njrich\\Desktop\\same-sex-migration\\W24_builds\\mm_f.csv") %>%
+  select(YEAR:time_leg_chunked) %>%
+  mutate(stateyear = paste(YEAR, state_name))
+W24_mm_gen <- read_csv("C:\\Users\\njrich\\Desktop\\same-sex-migration\\W24_builds\\mm_gen.csv") %>%
+  select(YEAR:time_leg_chunked) %>%
+  mutate(stateyear = paste(YEAR, state_name))
 
-#next steps: RCompare new methodology, adjust whether states are dropped by lack of same-sex individuals or lack of same-sex migrants
+compared_mm_m <- rCompare(mod_F24_mm_m, W24_mm_m, keys = c("YEAR", "state_name"), roundDigits = 6)
+compared_mm_f <- rCompare(mod_F24_mm_f, W24_mm_f, keys = c("YEAR", "state_name"), roundDigits = 6)
+compared_mm_gen <- rCompare(mod_F24_mm_gen, W24_mm_gen, keys = c("YEAR", "state_name"), roundDigits = 6)
+
+mm_f_comp <- mod_F24_mm_f %>% select(stateyear) %>% filter(!(stateyear %in% W24_mm_f$stateyear))
+mm_m_comp <- mod_F24_mm_f %>% select(stateyear) %>% filter(!(stateyear %in% W24_mm_m$stateyear))
+mm_gen_comp <- mod_F24_mm_f %>% select(stateyear) %>% filter(!(stateyear %in% W24_mm_gen$stateyear))
+
+#rCompare notes (NOT with MM):
+#F24 f/m/gen is off by 10^-2 on all values - NOT BIG DEAL can be explained by x100 in original to get percent (and then rounding in testcode version by rCompare)
+#F24 f has MT while W24 drops Montana - BIG DEAL data_cleaning version of code has bug in it - "MT" column got pushed after "WY" column so was dropped on "do the division"/select step (as of 9/20/24 line 458)
+
+#still need to reconcile with MM what is dropped (on state-years and count LGBT individuals (hmm census method differences too)))
+
+#next steps: fix if statement/decide NA dropping(see MM pages 5-methods- and 8-footnote on dropping and 9- some summary stats/table 1), re-run regression code see if any differences, adjust census same-sex counts to get closer to MM (part of adjusting whether states are dropped by lack of same-sex individuals or lack of same-sex migrants)
 
 
 #use clean_names?
